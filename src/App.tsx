a3e11f955480f187589f980d3d0655898973a080
axl0926 <song22092000@gmail.com>
2024-09-30 03:01:44 +0900

    config 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-
+import Navbar from "@components/layout/navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
 function App() {
     return (
-        <div>init</div>
+        <div className="flex ">
+            <Navbar />
+            <Routes>
+                <Route path="/schedule" element={<div>schedule</div>} />
+                <Route path="/task" element={<div>task</div>} />
+                <Route path="/news" element={<div>neews</div>} />
+            </Routes>
+        </div>
     );
 }
 
